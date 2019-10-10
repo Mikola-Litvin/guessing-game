f4dc0bf6d00d266fe0709b2d2ade180cfcfaff6e
Nikolai Shkrabkov <grr_by@mail.ru>
2019-10-10 15:51:08 +0300

    feat: add function code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,37 @@
 class GuessingGame {
-    constructor() {}
-
+    arr = []
+    minRange = 0
+    maxRange
+    middle
+    result
+  
+    constructor() {
+    }
+  
     setRange(min, max) {
-
+  
+      for (let i = min; i <= max; i++) {
+        this.arr.push(i);
+      }
+  
+      this.maxRange = this.arr.length - 1;
+  
     }
-
+  
     guess() {
-
+  
+      this.middle = Math.round((this.minRange + this.maxRange) / 2);
+      this.result = this.arr[this.middle];
+      return this.result;
     }
-
+  
     lower() {
-
+  
+      this.maxRange = this.middle;
     }
-
+  
     greater() {
-
+      this.minRange = this.middle;
     }
 }
 
